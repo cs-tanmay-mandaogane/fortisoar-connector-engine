a70a7d7d28f66b249c6bb34035d6485653ae099c
cs-tanmay-mandaogane <tsharad@fortinet-us.com>
2022-12-08 12:52:01 +0530

    revert

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ import os
 
 build_num = os.environ.get("BUILD_NUMBER", 1)
 
-
 setup(
     name='fortisoar-connector-engine',
     packages=find_packages(include=['connectors', 'connectors.core', 'connectors.scripts', 'integrations']),
